9fe84abc6d38d952e1cf6c5bff40217a2d156bf1
Dilara Akdeniz <dilaraakdeniz@Dilara-MacBook-Pro.local>
2025-08-16 22:40:30 +0300

    initialized

---
+++ Content/Lesson1.swift
@@ -0,0 +1,8 @@
+//
+//  Lesson1.swift
+//  Content
+//
+//  Created by Dilara Akdeniz on 16.08.2025.
+//
+
+import Foundation
---
+++ Content/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Content
+//
+//  Created by Dilara Akdeniz on 16.08.2025.
+//
+
+import Foundation
+
+print("Hello, World!")
+
